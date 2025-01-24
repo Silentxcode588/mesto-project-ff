1b941c718e00001d6bb92ebcb626579312c06fcc
Minnebaev Airat <airat.minnebaev2012@yandex.ru>
2025-01-25 01:21:57 +0300

    ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-
+function createCard(card, deleteCard) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true); //клонируем карточку
+  const imageCard = cardElement.querySelector(".card__image"); // Настройка изображения
+  imageCard.src = card.link;
+  imageCard.alt = card.alt;
+  cardElement.querySelector(".card__title").textContent = card.name; // Настройка заголовка
+  const deleteButton = cardElement.querySelector(".card__delete-button"); //Обработчик клика по кнопке удаления
+  deleteButton.addEventListener("click", deleteCard);
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function deleteOneCard(evt) {
+  evt.target.closest(".card").remove();
+}
+// @todo: Вывести карточки на страницу.
+initialCards.forEach((card) => {
+  const cardElement = createCard(card, deleteOneCard);
+  cardContainer.append(cardElement);
+});
