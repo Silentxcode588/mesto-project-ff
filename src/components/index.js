19438074b8b75d259d9ec43fb38eaae25908b3ec
Airat Minnebaev <Airat@MacBook-Air-admin.local>
2025-03-24 18:32:21 +0300

    submit_form_1.4yes

--- src/components/index.js
+++ src/components/index.js
@@ -49,6 +49,32 @@ initialCards.forEach((card) => {
   }
 
 
+// Находим форму в DOM
+const formElement = document.querySelector('.popup__form[name="edit-profile"]');// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = document.querySelector('.popup__input_type_name');// или form.elements.name;
+const jobInput = document.querySelector('.popup__input_type_description');  // или form.elements.description;
 
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+const nameValue = nameInput.value;
+const jobValue = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+    const profileTitle = document.querySelector('.profile__title');
+    const profileDescription = document.querySelector('.profile__description');
+    // Вставьте новые значения с помощью textContent
+    profileTitle.textContent = nameValue;
+    profileDescription.textContent = jobValue;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
 
 
